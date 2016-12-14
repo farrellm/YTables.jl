7da7b2eb87c0509aacbba612eacc0cabec756d8a
cth <christiantheilhave@gmail.com>
2016-12-14 14:55:53 +0100

    handle NA values in table

--- src/YTables.jl
+++ src/YTables.jl
@@ -12,6 +12,7 @@ abstract YStyle
 
 
 immutable LatexStyle <: YStyle
+    na::String
     embed_in_table::Bool
     tabular::String
     align::Dict{Symbol, String}
@@ -20,6 +21,7 @@ end
 
 
 immutable OrgStyle <: YStyle
+    na::String
 end
 
 
@@ -30,21 +32,22 @@ end
 
 
 ## constructors
-type_align(_::Number) = "r"
-type_align(_::String) = "l"
+#type_align(_::Number) = "r"
+#type_align(_::String) = "l"
+
+type_align{T<:Number}(_::DataArrays.DataArray{T,1}) = "r"
+type_align{T<:String}(_::DataArrays.DataArray{T,1}) = "l"
 
 
 make_align(data) =
-    Dict(n => type_align(data[1, n]) for n = names(data))
+    Dict(n => type_align(data[:, n]) for n = names(data))
 
-
-type_format(_::Integer) = v -> @sprintf("%d", v)
-type_format(_::Real)    = v -> @sprintf("%.2f", v)
-type_format(_::String)  = v -> @sprintf("%s", v)
-
+type_format{T<:Integer}(_::DataArrays.DataArray{T,1}) = v -> @sprintf("%d", v)
+type_format{T<:Real}(_::DataArrays.DataArray{T,1})    = v -> @sprintf("%.2f", v)
+type_format{T<:String}(_::DataArrays.DataArray{T,1})  = v -> @sprintf("%s", v)
 
 make_format(data) =
-    Dict(n => type_format(data[1, n]) for n in names(data))
+    Dict(n => type_format(data[:, n]) for n in names(data))
 
 
 formatter(f::String) =   @eval v -> @sprintf($f, v)
@@ -55,23 +58,24 @@ formatters(fs) =
     [formatter(v) for (k, v) = fs]
 
 
-latex(data; embed_in_table::Bool=true, tabular::String="tabular", alignment=Dict(), format=Dict()) =
-    YTable(data, LatexStyle(embed_in_table,
+latex(data; embed_in_table::Bool=true, tabular::String="tabular",na::String="-",alignment=Dict(), format=Dict()) =
+    YTable(data, LatexStyle(na,
+                            embed_in_table,
                             tabular,
                             merge(make_align(data), alignment),
                             merge(make_format(data), format)))
 
 
-org(data) = YTable(data, OrgStyle())
+org(data ; na::String="NA") = YTable(data, OrgStyle(na))
 
 
 ## printing
-format_row(row, format::Dict{Symbol, Function}) =
-    [string(format[n](row[n])) for n in names(row)]
+format_row(row, format::Dict{Symbol, Function}, na::String) =
+    [string(isna(row[n]) ? na : format[n](row[n])) for n in names(row)]
 
-function show_rows(io, data, format, pre, sep, post)
+function show_rows(io, data, format, na, pre, sep, post)
     for r = eachrow(data)
-        println(io, pre, join(format_row(r, format), sep), post)
+        println(io, pre, join(format_row(r, format, na), sep), post)
     end
 end
 
@@ -90,7 +94,7 @@ function show_table(io::IO, data::DataFrame, style::LatexStyle)
     println(io, "    \\hline")
     println(io, "    ", join(map(string, names(data)), " & "), " \\\\")
     println(io, "    \\hline")
-    show_rows(io, data, style.format, "    ", " & ", " \\\\")
+    show_rows(io, data, style.format, style.na, "    ", " & ", " \\\\")
     println(io, "    \\hline")
     println(io, "  \\end{tabular}")
     if style.embed_in_table
@@ -104,9 +108,9 @@ function show_table(io::IO, data::DataFrame, style::OrgStyle)
     widths = Dict(n => max(length(string(n)), vs[1]) for (n,vs) = eachcol(valwidths))
     format = Dict(n => formatter("%$v\s") for (n,v) = widths)
     ns = DataFrame(convert(Array{Any, 1}, [[n] for n in names(data)]), names(data))
-    show_rows(io, ns, format, "| ", " | ", " |")
+    show_rows(io, ns, format, style.na, "| ", " | ", " |")
     println(io, "|-", join([repeat("-", widths[n]) for n = names(data)], "-+-"), "-|")
-    show_rows(io, data, format, "| ", " | ", " |")
+    show_rows(io, data, format, style.na, "| ", " | ", " |")
 end
 
 
